96e22d097139405633a7eb7e693a69f816512516
Acreate <572646005@qq.com>
2025-06-03 10:05:51 +0800

    显示窗口

---
+++ srcs/cpp/MagicRenderView/srcs/main.cpp
@@ -0,0 +1,9 @@
+﻿#include <QApplication>
+#include <QMainWindow>
+int main( int argc, char *argv[ ] ) {
+	QApplication app( argc, argv );
+	
+	QMainWindow mainwidget;
+	mainwidget.show(  );
+	return app.exec( );
+}
