af2a20def43ea9922880be730ef78eb45851fe6e
Acreate <572646005@qq.com>
2025-06-03 09:43:17 +0800

    初始化

---
+++ srcs/MagicRenderView/srcs/main.cpp
@@ -0,0 +1,12 @@
+﻿
+#include "node/ui/windows/magicRenderViewWindow.h"
+#include "setting/appInstance.h"
+
+int main( int argc, char *argv[ ] ) {
+	cyl::app::AppInstance app( argc, argv );
+	
+	cyl::ui::MagicRenderViewWindow mainwidget;
+	mainwidget.show( );
+	app.setWindowToIndexScreenCentre( 0 );
+	return app.exec( );
+}
