6cda456a12d5bcec830bc8c89d59d9d460fcff89
Acreate <572646005@qq.com>
2025-06-04 22:27:59 +0800

    改进鼠标操作流程

--- srcs/cpp/MagicRenderView/srcs/qt/widget/nodeGraph.cpp
+++ srcs/cpp/MagicRenderView/srcs/qt/widget/nodeGraph.cpp
@@ -9,31 +9,49 @@ NodeGraph::NodeGraph( QWidget *parent, Qt::WindowFlags f ): QWidget( parent, f )
 	mousePosLabel = new QLabel( this );
 	mousePosLabel->setPixmap( QPixmap::fromImage( QImage( ":/images/add_node.png" ) ) );
 	mousePosLabel->hide( );
+	setMouseTracking( true );
+	mouseEventStatus = MouseEventType::Init;
 }
 NodeGraph::~NodeGraph( ) {
 }
 void NodeGraph::mouseReleaseEvent( QMouseEvent *event ) {
-	// 鼠标按键
-	Qt::MouseButton mouseButton = event->button( );	
-	// 鼠标全局位置
-	QPoint cursorPos = QCursor::pos( );
-	// 鼠标窗口位置
-	QPoint currentMouseInWidgetPos = event->pos( );
-	switch( mouseButton ) {
-		case Qt::RightButton : // 使用配置的位置显示菜单
-			nodeMenu->move( cursorPos );
-			nodeMenu->show( );
-			break;
-		case Qt::LeftButton : // 配置位置
-			if( mousePosLabel->isHidden( ) ) {
-				mousePosLabel->show( );
-				mousePosLabel->move( currentMouseInWidgetPos );
-			} else if( mousePosLabel->geometry( ).contains( currentMouseInWidgetPos ) ) {
-				nodeMenu->move( cursorPos );
-				nodeMenu->show( );
-			} else
-				mousePosLabel->move( currentMouseInWidgetPos );
+	switch( mouseEventStatus ) {
+		/// 左键菜单
+		case MouseEventType::Press : {
+			// 鼠标按键
+			Qt::MouseButton mouseButton = event->button( );
+			// 鼠标全局位置
+			QPoint cursorPos = QCursor::pos( );
+			// 鼠标窗口位置
+			QPoint currentMouseInWidgetPos = event->pos( );
+			switch( mouseButton ) {
+				case Qt::RightButton : // 使用配置的位置显示菜单
+					nodeMenu->move( cursorPos );
+					nodeMenu->show( );
+					break;
+				case Qt::LeftButton : // 配置位置
+					if( mousePosLabel->isHidden( ) ) {
+						mousePosLabel->show( );
+						mousePosLabel->move( currentMouseInWidgetPos );
+					} else if( mousePosLabel->geometry( ).contains( currentMouseInWidgetPos ) ) {
+						nodeMenu->move( cursorPos );
+						nodeMenu->show( );
+					} else
+						mousePosLabel->move( currentMouseInWidgetPos );
 
+					break;
+			}
+			break;
+		}
+		// todo : 操作被选中的节点
+		case MouseEventType::Move :
 			break;
 	}
+	mouseEventStatus = MouseEventType::Release;
+}
+void NodeGraph::mouseMoveEvent( QMouseEvent *event ) {
+	mouseEventStatus = MouseEventType::Move;
+}
+void NodeGraph::mousePressEvent( QMouseEvent *event ) {
+	mouseEventStatus = MouseEventType::Press;
 }
--- srcs/cpp/MagicRenderView/srcs/qt/widget/nodeGraph.h
+++ srcs/cpp/MagicRenderView/srcs/qt/widget/nodeGraph.h
@@ -7,17 +7,27 @@ class QLabel;
 class NodeAddMenu;
 class NodeGraph : public QWidget {
 	Q_OBJECT;
+private: // 私有类型
+	enum class MouseEventType {
+		Init, // 初始化
+		Press, // 按下
+		Release, // 释放
+		Move,// 移动
+	};
 protected:
 	/// @brief 节点添加菜单
-	NodeAddMenu* nodeMenu;
+	NodeAddMenu *nodeMenu;
 	/// @brief 用于显示鼠标位置
 	QLabel *mousePosLabel;
-
+	/// @brief 当前鼠标状态
+	MouseEventType mouseEventStatus;
 public:
 	NodeGraph( QWidget *parent = nullptr, Qt::WindowFlags f = Qt::WindowFlags( ) );
-	~NodeGraph()override;
+	~NodeGraph( ) override;
 protected:
 	void mouseReleaseEvent( QMouseEvent *event ) override;
+	void mouseMoveEvent( QMouseEvent *event ) override;
+	void mousePressEvent( QMouseEvent *event ) override;
 };
 
 #endif // NODEGRAPH_H_H_HEAD__FILE__
