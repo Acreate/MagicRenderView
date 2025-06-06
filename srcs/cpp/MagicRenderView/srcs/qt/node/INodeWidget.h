1558de7a81d475fa210fead9b5b0510c91852da4
Acreate <572646005@qq.com>
2025-06-06 19:32:45 +0800

    改进节点成员

--- srcs/cpp/MagicRenderView/srcs/qt/node/INodeWidget.h
+++ srcs/cpp/MagicRenderView/srcs/qt/node/INodeWidget.h
@@ -18,8 +18,13 @@ public:
 	/// @brief 连接到目标节点
 	/// @param target_node 目标节点对象
 	/// @param link_target_name 目标变量名称
+	/// @param link_result 链接结果返回，成功返回 0
 	/// @return 如果该节点为单向节点，那么返回上次链接对象，或返回断开链接对象
-	virtual const INodeWidget * linkTarget( INodeWidget *target_node, const QString &link_target_name ) const = 0;
+	virtual const INodeWidget * linkTarget( INodeWidget *target_node, const QString &link_target_name, int32_t &link_result ) const = 0;
+	/// @brief 获取链接源列表
+	/// @return 链接源列表
+	virtual std_vector_pairt< INodeWidget *, QString > getLinkSourceNode( ) const;
+
 };
 
 #endif // INODEWIDGET_H_H_HEAD__FILE__
