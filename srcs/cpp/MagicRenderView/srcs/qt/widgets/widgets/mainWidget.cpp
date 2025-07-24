6c24649c034a7aabfcc4fcf1412997d429a772ac
Acreate <1552397900@qq.com>
2025-07-24 18:05:48 +0800

    改进比例权重问题

--- srcs/cpp/MagicRenderView/srcs/qt/widgets/widgets/mainWidget.cpp
+++ srcs/cpp/MagicRenderView/srcs/qt/widgets/widgets/mainWidget.cpp
@@ -13,7 +13,7 @@ MainWidget::MainWidget( QWidget *parent, Qt::WindowFlags flags ): QWidget( paren
 	nodeListWidget = new NodeListScrollAreasWidget( this );
 	nodeRenderWidget = new NodeRenderScrollAreasWidget( this );
 	nodeScriptsWidget = new NodeScriptsScrollAreasWidget( this );
-	
+
 	renderLayout.first = "Application/MainWindow/MainWidget/NodeListScrollAreasWidget";
 	renderLayout.second = 8;
 	renderLayout.second = appInstance->value( renderLayout.first, renderLayout.second ).toLongLong( );
@@ -25,11 +25,11 @@ MainWidget::MainWidget( QWidget *parent, Qt::WindowFlags flags ): QWidget( paren
 	nodeScriptsLayout.first = "Application/MainWindow/MainWidget/NodeScriptsScrollAreasWidget";
 	nodeScriptsLayout.second = 3;
 	nodeScriptsLayout.second = appInstance->value( nodeScriptsLayout.first, nodeScriptsLayout.second ).toLongLong( );
-	
+
 	nodeListWidget->show( );
 	nodeRenderWidget->show( );
 	nodeScriptsWidget->show( );
-	
+
 	updateResize( );
 }
 MainWidget::~MainWidget( ) {
@@ -50,17 +50,19 @@ void MainWidget::updateResize( ) {
 	int currentWidth = rect.width( );
 	int currentHeight = rect.height( );
 	int mulBase = currentHeight / ( renderLayout.second + nodeListLayout.second + nodeScriptsLayout.second );
-	int h = mulBase * renderLayout.second;
+
+	int nodeListWidgetHeight = mulBase * nodeListLayout.second;
+	nodeListWidget->setFixedSize( currentWidth, nodeListWidgetHeight );
+
+	size_t nodeScriptsWidgetHeight = mulBase * nodeScriptsLayout.second;
+	nodeScriptsWidget->setFixedSize( currentWidth, nodeScriptsWidgetHeight );
+
+	size_t nodeRenderWidgetHeight = currentHeight - nodeListWidgetHeight - nodeScriptsWidgetHeight;
+	nodeRenderWidget->setFixedSize( currentWidth, nodeRenderWidgetHeight );
 
 	nodeRenderWidget->move( 0, 0 );
-	nodeRenderWidget->setFixedSize( currentWidth, h );
-
-	nodeListWidget->move( 0, h );
-	size_t h1 = mulBase * nodeListLayout.second;
-	nodeListWidget->setFixedSize( currentWidth, h1 );
-	h = h + h1;
-	nodeScriptsWidget->move( 0, h );
-	nodeScriptsWidget->setFixedSize( currentWidth, currentHeight - h );
+	nodeListWidget->move( 0, nodeRenderWidgetHeight );
+	nodeScriptsWidget->move( 0, nodeRenderWidgetHeight + nodeListWidgetHeight );
 
 }
 void MainWidget::resizeEvent( QResizeEvent *event ) {
