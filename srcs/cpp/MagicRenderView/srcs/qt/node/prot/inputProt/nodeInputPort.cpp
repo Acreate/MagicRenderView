f90dee6cf9d3741939b612c76c17c65fb3f8d324
Acreate <1552397900@qq.com>
2025-09-16 01:17:02 +0800

    修复已知问题

--- srcs/cpp/MagicRenderView/srcs/qt/node/prot/inputProt/nodeInputPort.cpp
+++ srcs/cpp/MagicRenderView/srcs/qt/node/prot/inputProt/nodeInputPort.cpp
@@ -15,12 +15,14 @@ void NodeInputPort::updateProtLayout( ) {
 	int fontHeight = fontMetrics.height( ) + drawHeight;
 	drawHeight = fontHeight - fontMetrics.descent( ) - drawHeight;
 	QImage ico = QImage( ":/ico/info_node.png" ).scaledToHeight( fontHeight );
-	int imageWidth = width + ico.width( );
+	int icoWidth = ico.width( );
+	int imageWidth = width + icoWidth;
 	nodePortRender = nodePortRender.scaled( imageWidth, fontHeight );
 	QPainter painter( &nodePortRender );
 	painter.setFont( font );
 	painter.drawImage( 0, 0, ico );
-	painter.drawText( ico.width( ), drawHeight, title );
+	painter.drawText( icoWidth, drawHeight, title );
 	painter.end( );
 	nodeSize = QSize( imageWidth, fontHeight );
+	nodePortRender.save( "./NodeInputPort.png" );
 }
