cbae793b8a6fd783cfdb83f18c64486423cbbdaa
Acreate <1552397900@qq.com>
2025-09-28 07:09:52 +0800

    完善接口

---
+++ srcs/cpp/MagicRenderView/srcs/qt/varType/I_Serialization‌.h
@@ -0,0 +1,10 @@
+﻿#ifndef I_SERIALIZATION__H_H_HEAD__FILE__
+#define I_SERIALIZATION__H_H_HEAD__FILE__
+#pragma once
+
+class I_Serialization‌ {
+	friend class I_Conver;
+	virtual bool appendVarType()
+};
+
+#endif // I_SERIALIZATION__H_H_HEAD__FILE__
--- srcs/cpp/MagicRenderView/srcs/qt/varType/I_Type.cpp
+++ srcs/cpp/MagicRenderView/srcs/qt/varType/I_Type.cpp
@@ -1,10 +1,3 @@
 ﻿#include "./I_Type.h"
 
 #include "I_Stack.h"
-
-I_Type::I_Type( const std_shared_ptr< I_Stack > &create_obj_stack, void *var_p ) : createObjStack( create_obj_stack ), typeInfo( create_obj_stack.get( )->getGenerateTypeInfo( ) ), p( var_p ) {
-
-}
-I_Type::~I_Type( ) {
-	createObjStack->deleteTarget( typeInfo, p );
-}
