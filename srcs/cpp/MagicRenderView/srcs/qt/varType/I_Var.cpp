70595b577f903c84a16355fca1b058d324de0b00
Acreate <1552397900@qq.com>
2025-10-01 18:38:13 +0800

    改进 i_var

--- srcs/cpp/MagicRenderView/srcs/qt/varType/I_Var.cpp
+++ srcs/cpp/MagicRenderView/srcs/qt/varType/I_Var.cpp
@@ -1,1 +1,41 @@
 ﻿#include "I_Var.h"
+
+#include "I_Type.h"
+I_Var::I_Var( const std_shared_ptr< I_Type > &type_info, const QString &var_name ) : typeInfo( type_info ), varName( var_name ) {
+	I_Type *element = type_info.get( );
+	auto &create = element->getCreate( );
+	if( create )
+		create( p );
+	else
+		p = nullptr;
+}
+I_Var::I_Var( const I_Var &other ) {
+	auto &create = other.typeInfo->getCreate( );
+	size_t memorySize = other.typeInfo->getMemorySize( );
+	I_Type *element = new I_Type( other.typeInfo->getTypeInfo( ), memorySize, other.typeInfo->getRelease( ), create );
+	typeInfo = std_shared_ptr< I_Type >( element );
+	p = nullptr;
+	if( create == nullptr || memorySize == 0 )
+		return;
+	create( p );
+	memcpy( p, other.p, memorySize );
+}
+I_Var & I_Var::operator=( const I_Var &other ) {
+	if( this == &other )
+		return *this;
+	auto &create = other.typeInfo->getCreate( );
+	size_t memorySize = other.typeInfo->getMemorySize( );
+	I_Type *element = new I_Type( other.typeInfo->getTypeInfo( ), memorySize, other.typeInfo->getRelease( ), create );
+	typeInfo = std_shared_ptr< I_Type >( element );
+	if( create && memorySize != 0 ) {
+		create( p );
+		memcpy( p, other.p, memorySize );
+	} else
+		p = nullptr;
+	return *this;
+}
+I_Var::~I_Var( ) {
+	if( p )
+		typeInfo->getRelease( )( p );
+	p = nullptr;
+}
--- srcs/cpp/MagicRenderView/srcs/qt/varType/I_Var.h
+++ srcs/cpp/MagicRenderView/srcs/qt/varType/I_Var.h
@@ -10,7 +10,14 @@ protected:
 	std_shared_ptr< I_Type > typeInfo;
 	void *p;
 	QString varName;
-	I_Var( ) { }
+public:
+	I_Var( const std_shared_ptr< I_Type > &type_info, const QString &var_name );
+	I_Var( const I_Var &other );
+	I_Var & operator=( const I_Var &other );
+	virtual ~I_Var( );
+	virtual const std_shared_ptr< I_Type > & getTypeInfo( ) const { return typeInfo; }
+	virtual void * getP( ) const { return p; }
+	virtual const QString & getVarName( ) const { return varName; }
 };
 
 #endif // I_VAR_H_H_HEAD__FILE__
