7014009cd6d2152125e847404cfcfccbc08173a9
Acreate <1552397900@qq.com>
2025-10-14 17:02:51 +0800

    解耦函数功能

--- srcs/cpp/MagicRenderView/srcs/qt/node/director/nodeDirector.cpp
+++ srcs/cpp/MagicRenderView/srcs/qt/node/director/nodeDirector.cpp
@@ -1,6 +1,18 @@
 ﻿#include "nodeDirector.h"
 
+#include <qmenu.h>
+
+#include "../../application/application.h"
+
+#include "../../generate/varGenerate.h"
+
 #include "../widgets/nodeItemInfoScrollAreaWidget.h"
+NodeItem * NodeDirector::NodeItemGenerateInfo::createNodeItem( const QString &dir_name, const QString &node_name ) {
+	bool cond = dirName == dir_name && nodeName == node_name;
+	if( cond == true )
+		return ( NodeItem * ) createTypeInstancePtr.get( )->getCreate( ).operator()( );
+	return nullptr;
+}
 bool NodeDirector::addManagementWidget( NodeItemInfoScrollAreaWidget *add_widget ) {
 	size_t count = nodeItemInfoScrollAreaWidgets.size( );
 	if( count != 0 ) {
@@ -18,13 +30,13 @@ bool NodeDirector::addManagementWidget( NodeItemInfoScrollAreaWidget *add_widget
 	} );
 	return true;
 }
-bool NodeDirector::removeManagementWidget( NodeItemInfoScrollAreaWidget *add_widget ) {
+bool NodeDirector::removeManagementWidget( NodeItemInfoScrollAreaWidget *del_widget ) {
 	size_t count = nodeItemInfoScrollAreaWidgets.size( );
 	if( count != 0 ) {
 		size_t index = 0;
 		auto data = nodeItemInfoScrollAreaWidgets.data( );
 		for( ; index < count; ++index )
-			if( data[ index ] == add_widget ) {
+			if( data[ index ] == del_widget ) {
 				data[ index ] = nullptr;
 				return true;
 			}
@@ -45,11 +57,151 @@ bool NodeDirector::linkUnInstallPort( NodeInputPort *input_port, NodeOutputPort 
 size_t NodeDirector::run( ) {
 	return 0;
 }
+bool NodeDirector::setContentWidget( MainWidget *main_widget ) {
+	if( main_widget == nullptr )
+		return false;
+	if( mainWidget == main_widget )
+		return true;
+	generateNodeItemInfos.clear( );
+	lastActiveNodeItem = nullptr;
+	auto clone = generateNodeItems;
+	generateNodeItems.clear( );
+	for( auto removeItem : clone )
+		delete removeItem;
+
+	mainWidget = main_widget;
+	applicationInstancePtr = Application::getApplicationInstancePtr( );
+
+	varGenerate = applicationInstancePtr->getVarGenerate( );
+	if( nodeItemCreateMenu )
+		delete nodeItemCreateMenu;
+	nodeItemCreateMenu = new QMenu( );
+
+	auto infos = varGenerate->getNodeItemSortMap( );
+	QString enumString;
+	for( auto &[ enumType,dirNamMap ] : infos ) {
+		if( NodeItem::getEnumName( enumType, enumString ) == false )
+			continue;
+		QMenu *nodeTypeMenu = nodeItemCreateMenu->addMenu( enumString );
+		for( auto &[ dir,nameMap ] : dirNamMap ) {
+			QMenu *dirMenu = nodeTypeMenu->addMenu( dir );
+			for( auto &[ className, typePtr ] : nameMap ) {
+				if( className.isEmpty( ) )
+					continue;
+				auto addAction = dirMenu->addAction( className );
+				auto nodeItemGenerateInfo = std_shared_ptr< NodeItemGenerateInfo >( new NodeItemGenerateInfo( dir, className, typePtr ) );
+				generateNodeItemInfos.emplace_back( nodeItemGenerateInfo );
+				connect( addAction, &QAction::triggered, [this, dir, className, typePtr]( ) {
+					createNodeItem( dir, className, typePtr );
+				} );
+			}
+		}
+	}
+
+	return true;
+}
+
+NodeItem * NodeDirector::createNodeItem( const QString &dir_name, const QString &node_name, const std_shared_ptr< I_Type > &itype_ptr ) {
+	NodeItem *nodeItem = ( NodeItem * ) itype_ptr->getCreate( ).operator()( );
+	if( nodeItem == nullptr )
+		return nullptr;
+	if( appendNodeItem( nodeItem ) )
+		return nodeItem;
+	delete nodeItem;
+	return nullptr;
+}
+
+size_t NodeDirector::appendNodeItem( NodeItem *new_node_item ) {
+
+	new_node_item->setMainWidget( mainWidget );
+	if( new_node_item->intPortItems( mainWidget ) == false )
+		return 0;
+	size_t count = generateNodeItems.size( );
+	auto data = generateNodeItems.data( );
+	size_t index = 0;
+	for( ; index < count; ++index )
+		if( data[ index ] == nullptr ) {
+			data[ index ] = new_node_item;
+			break;
+		}
+	if( index == count ) {
+		generateNodeItems.emplace_back( new_node_item );
+		++count;
+		data = generateNodeItems.data( ); // 更新基址
+	}
+	size_t checkCode = count;
+	for( ; index < count; ++index )
+		if( data[ index ]->generateCode != ( index + 1 ) ) { // 掉链子了
+			checkCode = index + 1;
+			index = 0; // 重新遍历
+			for( ; index < count; ++index )
+				if( data[ index ]->generateCode == checkCode ) {
+					index = checkCode + 1;
+					checkCode = count;
+					break;
+				}
+			if( index == count )
+				break;
+
+		}
+	new_node_item->generateCode = checkCode;
+	connect( new_node_item, &NodeItem::releaseThiNodeItem, [this] ( NodeItem *release_node_item ) {
+				size_t count = generateNodeItems.size( );
+				if( count == 0 )
+					return;
+				auto data = generateNodeItems.data( );
+				size_t index = 0;
+				for( ; index < count; ++index )
+					if( data[ index ] == release_node_item ) {
+						data[ index ] = nullptr;
+						break;
+					}
+			}
+		);
+	lastActiveNodeItem = new_node_item;
+	return new_node_item->generateCode;
+}
+
 NodeDirector::~NodeDirector( ) {
 	size_t count = nodeItemInfoScrollAreaWidgets.size( );
+	size_t index;
+	if( count ) {
+		index = 0;
+		auto data = nodeItemInfoScrollAreaWidgets.data( );
+		for( ; index < count; ++index )
+			if( data[ index ] != nullptr )
+				delete data[ index ];
+	}
+
+	count = generateNodeItems.size( );
+	if( count ) {
+		index = 0;
+		auto data = generateNodeItems.data( );
+		for( ; index < count; ++index )
+			if( data[ index ] != nullptr )
+				delete data[ index ];
+	}
+
+	if( nodeItemCreateMenu )
+		delete nodeItemCreateMenu;
+}
+
+bool NodeDirector::getNodeItemRender( QImage &result_render_image, const QPoint &offset ) const {
+	return true;
+}
+NodeItem::Click_Type NodeDirector::getClickNodeItem( NodeItem *&result_node_item ) {
+	return getClickNodeItem( mainWidget->mapFromGlobal( QCursor::pos( ) ), result_node_item );
+}
+NodeItem::Click_Type NodeDirector::getClickNodeItem( const QPoint &click_pos, NodeItem *&result_node_item ) {
+
+	size_t count = generateNodeItems.size( );
+	auto data = generateNodeItems.data( );
 	size_t index = 0;
-	auto data = nodeItemInfoScrollAreaWidgets.data( );
 	for( ; index < count; ++index )
-		if( data[ index ] != nullptr )
-			delete data[ index ];
+		if( data[ index ] == nullptr ) {
+
+			break;
+		}
+
+	return NodeItem::Click_Type::None;
 }
--- srcs/cpp/MagicRenderView/srcs/qt/node/director/nodeDirector.h
+++ srcs/cpp/MagicRenderView/srcs/qt/node/director/nodeDirector.h
@@ -5,6 +5,8 @@
 
 #include "../../../alias/type_alias.h"
 
+#include "../item/nodeItem.h"
+
 class NodeItemInfoScrollAreaWidget;
 class NodeItem;
 class NodeEditorWidget;
@@ -13,17 +15,67 @@ class NodeInputPort;
 class NodeDirector : public QObject {
 	Q_OBJECT;
 protected:
+	class NodeItemGenerateInfo {
+		QString dirName;
+		QString nodeName;
+		std_shared_ptr< I_Type > createTypeInstancePtr;
+	public:
+		virtual ~NodeItemGenerateInfo( ) = default;
+		NodeItemGenerateInfo( const QString &dir_name, const QString &node_name, const std_shared_ptr< I_Type > &create_type_instance_ptr )
+			: dirName( dir_name ),
+			nodeName( node_name ),
+			createTypeInstancePtr( create_type_instance_ptr ) { }
+		virtual const QString & getDirName( ) const { return dirName; }
+		virtual const QString & getNodeName( ) const { return nodeName; }
+		virtual const std_shared_ptr< I_Type > & getCreateTypeInstancePtr( ) const { return createTypeInstancePtr; }
+		virtual bool isNodeType( const QString &dir_name, const QString &node_name ) {
+			return dirName == dir_name && nodeName == node_name;
+		}
+		virtual NodeItem * createNodeItem( const QString &dir_name, const QString &node_name );
+	};
+protected:
+	/// @brief 绑定的主窗口
+	MainWidget *mainWidget = nullptr;
+	/// @brief 节点实例编辑窗口
 	std_vector< NodeItemInfoScrollAreaWidget * > nodeItemInfoScrollAreaWidgets;
+	/// @brief 已经创建的实例
+	std_vector< NodeItem * > generateNodeItems;
+	/// @brief 节点生成实例对象列表
+	std_vector< std_shared_ptr< NodeItemGenerateInfo > > generateNodeItemInfos;
+	/// @brief 节点创建菜单
+	QMenu *nodeItemCreateMenu = nullptr;
+	/// @brief 当前进程实例
+	Application *applicationInstancePtr = nullptr;
+	/// @brief 对象生成实例
+	VarGenerate *varGenerate = nullptr;
+	/// @brief 最后激活的节点
+	NodeItem *lastActiveNodeItem = nullptr;
 protected:
 	virtual bool addManagementWidget( NodeItemInfoScrollAreaWidget *add_widget );
-	virtual bool removeManagementWidget( NodeItemInfoScrollAreaWidget *add_widget );
+	virtual bool removeManagementWidget( NodeItemInfoScrollAreaWidget *del_widget );
 public:
 	NodeDirector( QObject *parent = nullptr );
 	~NodeDirector( ) override;
+
+	virtual bool getNodeItemRender( QImage &result_render_image, const QPoint &offset = QPoint( 0, 0 ) ) const;
+	virtual bool getNodeItemRender( QImage &result_render_image ) const {
+		return getNodeItemRender( result_render_image, QPoint( 0, 0 ) );
+	}
+	virtual NodeItem::Click_Type getClickNodeItem( NodeItem * &result_node_item );
+	virtual NodeItem::Click_Type getClickNodeItem( const QPoint &click_pos, NodeItem * &result_node_item );
+	virtual QMenu * getNodeItemCraeteMenu( ) const {
+		return nodeItemCreateMenu;
+	}
 	virtual NodeItemInfoScrollAreaWidget * requestGetNodeEditorWidget( const type_info &request_type, NodeItem *request_node_item_ptr );
 	virtual bool linkInstallPort( NodeInputPort *input_port, NodeOutputPort *output_port );
 	virtual bool linkUnInstallPort( NodeInputPort *input_port, NodeOutputPort *output_port );
 	virtual size_t run( );
+	virtual bool setContentWidget( MainWidget *main_widget );
+	NodeItem * createNodeItem( const QString &dir_name, const QString &node_name, const std_shared_ptr< I_Type > &itype_ptr );
+	size_t appendNodeItem( NodeItem *new_node_item );
+	virtual MainWidget * getContentWidget( ) const {
+		return mainWidget;
+	}
 };
 
 #endif // NODEDIRECTOR_H_H_HEAD__FILE__
