ce107658b4fc22d6f22247f5db0d38547d0b2472
Acreate <1552397900@qq.com>
2025-10-24 00:42:17 +0800

    文件添加

---
+++ srcs/cpp/MagicRenderView/srcs/qt/widgets/generateListWidget/generateListScrollArea.h
@@ -0,0 +1,15 @@
+﻿#ifndef GENERATELISTSCROLLAREA_H_H_HEAD__FILE__
+#define GENERATELISTSCROLLAREA_H_H_HEAD__FILE__
+#pragma once
+#include <QScrollArea>
+
+class GenerateListWidget;
+class GenerateListScrollArea : public QScrollArea {
+	Q_OBJECT;
+	GenerateListWidget* generateListWidget;
+public:
+	GenerateListScrollArea( QWidget *parent = nullptr );
+	virtual GenerateListWidget * getGenerateListWidget( ) const { return generateListWidget; }
+};
+
+#endif // GENERATELISTSCROLLAREA_H_H_HEAD__FILE__
