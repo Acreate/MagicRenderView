bb5f98c9e5944de0898e03219a115a0b99869d87
Acreate <1552397900@qq.com>
2025-11-05 17:55:56 +0800

    修复已知问题

--- srcs/cpp/MagicRenderView/srcs/qt/node/director/nodeItemBuilderModule.cpp
+++ srcs/cpp/MagicRenderView/srcs/qt/node/director/nodeItemBuilderModule.cpp
@@ -23,29 +23,35 @@ std_vector< NodeItemInfo * > NodeItemBuilderModule::findEndAtStartNode( NodeItem
 }
 std_vector< NodeItemBuilderModule * > NodeItemBuilderModule::generateModuleVector( NodeItemInfo **node_item_info_array_ptr, const size_t &node_item_info_array_count ) {
 
-	std_vector< NodeItemBuilderModule * > result;
-
-	size_t index = 0;
-	NodeItemBuilderModule *element;
-
-	for( ; index < node_item_info_array_count; ++index )
+	std_vector< NodeItemBuilderModule * > result; // 返回的模型列表
+	NodeItemBuilderModule *element; // 临时操作模块对象指针
+	NodeItemBuilderModule **resultArrayPtr; // 遍历用的返回模型列表序列指针
+	std_vector< NodeItemInfo * > endAtStartNode; // 保存返回用的开始节点序列
+	NodeItemInfo **endAtStartNodeArrayPtr;// 遍历保存返回用的开始节点序列
+	NodeItemInfo **existArrayPtr;// 遍历已经生成的模块保存返回用的开始节点序列
+	size_t index; // 输入数组下标
+	size_t resultIndex; // 返回模块序列下标
+	size_t resultCount; // 返回模块数量
+	size_t endAtStartNodeIndex; // 返回的开始节点下标
+	size_t endAtStartNodeCount; // 返回的开始节点个数
+	size_t existIndex; // 遍历模块的开始节点下标
+	size_t existStartCount; // 遍历模块的开始节点个数
+	for( index = 0; index < node_item_info_array_count; ++index )
 		if( node_item_info_array_ptr[ index ]->nodeItem->getNodeMetaType( ) == nodeItemEnum::Node_Item_Type::End ) {
-			auto endAtStartNode = findEndAtStartNode( node_item_info_array_ptr[ index ] );
+			endAtStartNode = findEndAtStartNode( node_item_info_array_ptr[ index ] );
 			if( endAtStartNode.size( ) == 0 )
 				continue;
-			size_t resultCount = result.size( );
-			auto resultArrayPtr = result.data( );
-			size_t resultIndex = 0;
-			size_t endAtStartNodeCount = endAtStartNode.size( );
-			auto endAtStartNodeArrayPtr = endAtStartNode.data( );
-			size_t endAtStartNodeIndex = 0;
+			resultCount = result.size( );
+			resultArrayPtr = result.data( );
+			endAtStartNodeCount = endAtStartNode.size( );
+			endAtStartNodeArrayPtr = endAtStartNode.data( );
 			element = nullptr;
-			for( ; resultIndex < resultCount; ++resultIndex ) {
+			for( resultIndex = 0, endAtStartNodeIndex = 0; resultIndex < resultCount; ++resultIndex ) {
 				element = resultArrayPtr[ resultIndex ];
-				size_t existStartCount = element->startNodeItemInfoVector.size( );
-				auto existArrayPtr = element->startNodeItemInfoVector.data( );
-				size_t existIndex = 0;
-				for( ; existIndex < existStartCount; ++existIndex ) {
+				existStartCount = element->startNodeItemInfoVector.size( );
+				existArrayPtr = element->startNodeItemInfoVector.data( );
+
+				for( existIndex = 0; existIndex < existStartCount; ++existIndex ) {
 					for( endAtStartNodeIndex = 0; endAtStartNodeIndex < endAtStartNodeCount; ++endAtStartNodeIndex )
 						if( existArrayPtr[ existIndex ] == endAtStartNodeArrayPtr[ endAtStartNodeIndex ] )
 							break;
@@ -62,11 +68,12 @@ std_vector< NodeItemBuilderModule * > NodeItemBuilderModule::generateModuleVecto
 							continue;
 						element->startNodeItemInfoVector.emplace_back( endAtStartNodeArrayPtr[ endAtStartNodeIndex ] );
 					}
-					element = nullptr;
+
 					break;
 				}
+				element = nullptr;
 			}
-			if( element == nullptr )
+			if( element != nullptr )
 				continue;
 			element = new NodeItemBuilderModule;
 			element->startNodeItemInfoVector = endAtStartNode;
