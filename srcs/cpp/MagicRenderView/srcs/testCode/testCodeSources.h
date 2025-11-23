a3bdacdca8773d4dbb124faa3617f5373b9de66c
Acreate <1552397900@qq.com>
2025-11-23 09:52:50 +0800

    改进测试功能

---
+++ srcs/cpp/MagicRenderView/srcs/testCode/testCodeSources.h
@@ -0,0 +1,10 @@
+﻿#ifndef file_mac_H_HEAD__FILE__
+#define file_mac_H_HEAD__FILE__
+#pragma once
+
+class TestCodeSources {
+public:
+	static void testVarGener( );
+};
+
+#endif // file_mac_H_HEAD__FILE__
