dc3bae8055e1a2ff5c7d1f5731aed074f7848493
Acreate <1552397900@qq.com>
2025-12-27 15:24:42 +0800

    添加 ApplicationMenuStack 代码框架

--- srcs/cpp/MagicRenderView/srcs/menuStack/app/applicationMenuStack.cpp
+++ srcs/cpp/MagicRenderView/srcs/menuStack/app/applicationMenuStack.cpp
@@ -4,6 +4,9 @@
 #include <QToolBar>
 
 #include "../../app/application.h"
+#include "../../menu/app/action/normalApplicationAction.h"
+#include "../../menu/app/normalApplicationMenu.h"
+#include "../../menu/app/normalApplicationToolBar.h"
 ApplicationMenuStack::ApplicationMenuStack( QObject *parent ) : QObject( parent ) {
 
 }
@@ -13,11 +16,35 @@ bool ApplicationMenuStack::initStack( ) {
 	return true;
 }
 NormalApplicationToolBar * ApplicationMenuStack::getToolBar( const QString &tool_menu_name ) {
-	return nullptr;
+	NormalApplicationToolBar *fromTypeCreate = toolBarGeneratorArray.fromTypeCreate( tool_menu_name );
+	if( fromTypeCreate == nullptr ) {
+		fromTypeCreate = toolBarGeneratorArray.fromAliasTypeCreate( tool_menu_name );
+		if( fromTypeCreate == nullptr )
+			return nullptr;
+	}
+	if( fromTypeCreate->init( this ) == false )
+		return nullptr;
+	return fromTypeCreate;
 }
 NormalApplicationMenu * ApplicationMenuStack::getMenu( const QString &tool_menu_name ) {
-	return nullptr;
+	NormalApplicationMenu *fromTypeCreate = menuGeneratorArray.fromTypeCreate( tool_menu_name );
+	if( fromTypeCreate == nullptr ) {
+		fromTypeCreate = menuGeneratorArray.fromAliasTypeCreate( tool_menu_name );
+		if( fromTypeCreate == nullptr )
+			return nullptr;
+	}
+	if( fromTypeCreate->init( this ) == false )
+		return nullptr;
+	return fromTypeCreate;
 }
 NormalApplicationAction * ApplicationMenuStack::getAction( const QString &acton_name ) {
-	return nullptr;
+	NormalApplicationAction *fromTypeCreate = actionGeneratorArray.fromTypeCreate( acton_name );
+	if( fromTypeCreate == nullptr ) {
+		fromTypeCreate = actionGeneratorArray.fromAliasTypeCreate( acton_name );
+		if( fromTypeCreate == nullptr )
+			return nullptr;
+	}
+	if( fromTypeCreate->init( this ) == false )
+		return nullptr;
+	return fromTypeCreate;
 }
--- srcs/cpp/MagicRenderView/srcs/menuStack/app/applicationMenuStack.h
+++ srcs/cpp/MagicRenderView/srcs/menuStack/app/applicationMenuStack.h
@@ -15,7 +15,72 @@ class QToolBar;
 class ApplicationMenuStack : public QObject {
 	Q_OBJECT;
 protected:
+	template< typename TGeneratorType >
+	class Generator {
+	private:
+		std::function< TGeneratorType*( ) > generator;
+		QString typeName;
+		std::vector< QString > aliasTypeName;
+		QString *aliasTypeNameArrayPtr;
+		size_t aliasTypeNameArrayCount;
+	public:
+		virtual ~Generator( ) = default;
+		Generator( const std::function< TGeneratorType *( ) > &generator, const QString &type_name, const std::vector< QString > &alias_type_name )
+			: generator( generator ),
+			typeName( type_name ),
+			aliasTypeName( alias_type_name ) {
+			aliasTypeNameArrayPtr = aliasTypeName.data( );
+			aliasTypeNameArrayCount = aliasTypeName.size( );
+		}
+		virtual TGeneratorType * create( ) {
+			return generator( );
+		}
+		virtual bool isType( const QString &type_name ) {
+			return type_name == typeName;
+		}
+		virtual bool isAliasType( const QString &alisa_type_name ) {
+			for( size_t index = 0; index < aliasTypeNameArrayCount; ++index )
+				if( aliasTypeNameArrayPtr[ index ] == alisa_type_name )
+					return true;
+			return false;
+		}
+	};
+
+	template< typename TGeneratorType >
+	class GeneratorArray {
+	private:
+		std::vector< Generator< TGeneratorType > > normalrGeneratorVector;
+		Generator< TGeneratorType > *normalGeneratorArrayPtr;
+		size_t normalGeneratorArrayCount;
+	public:
+		virtual ~GeneratorArray( ) = default;
+		GeneratorArray( ) {
+			normalGeneratorArrayPtr = nullptr;
+			normalGeneratorArrayCount = 0;
+		}
+		virtual void appendGenerator( const Generator< TGeneratorType > &append_generator ) {
+			normalrGeneratorVector.emplace_back( append_generator );
+			normalGeneratorArrayPtr = normalrGeneratorVector.data( );
+			normalGeneratorArrayCount = normalrGeneratorVector.size( );
+		}
+		virtual TGeneratorType * fromTypeCreate( const QString &type_name, size_t index = 0 ) {
+			for( ; index < normalGeneratorArrayCount; index )
+				if( normalGeneratorArrayPtr[ index ].isType( type_name ) )
+					return normalGeneratorArrayPtr[ index ].create( );
+			return nullptr;
+		}
+		virtual TGeneratorType * fromAliasTypeCreate( const QString &type_name, size_t index = 0 ) {
+			for( ; index < normalGeneratorArrayCount; index )
+				if( normalGeneratorArrayPtr[ index ].isAliasType( type_name ) )
+					return normalGeneratorArrayPtr[ index ].create( );
+			return nullptr;
+		}
+	};
+protected:
 	Application *application;
+	GeneratorArray< NormalApplicationToolBar > toolBarGeneratorArray;
+	GeneratorArray< NormalApplicationMenu > menuGeneratorArray;
+	GeneratorArray< NormalApplicationAction > actionGeneratorArray;
 public:
 	ApplicationMenuStack( QObject *parent = nullptr );
 	virtual bool initStack( );
