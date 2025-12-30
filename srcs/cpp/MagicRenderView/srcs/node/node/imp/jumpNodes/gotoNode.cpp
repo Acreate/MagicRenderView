9166464d5a83b340122070b219b1413d0377f825
Acreate <1552397900@qq.com>
2025-12-30 12:42:50 +0800

    修复已知问题

--- srcs/cpp/MagicRenderView/srcs/node/node/imp/jumpNodes/gotoNode.cpp
+++ srcs/cpp/MagicRenderView/srcs/node/node/imp/jumpNodes/gotoNode.cpp
@@ -1,11 +1,12 @@
 ﻿#include "gotoNode.h"
 
 #include "../../../port/inputPort/interface/interFaceInputPort.h"
+#include "../../../port/inputPort/point/pointInputPort.h"
 #include "../../../port/inputPort/toPoint/toPointInputPort.h"
 
 bool GotoNode::initEx( MainWidget *parent ) {
 	initExCallFunction = [this] ( MainWidget *draw_node_widget ) {
-		if( appendInputPortType< InterFaceInputPort >( tr( "跳转" ) ) == nullptr )
+		if( appendInputPortType< PointInputPort >( tr( "跳转" ) ) == nullptr )
 			return false;
 		if( appendInputPortType< ToPointInputPort >( tr( "跳到定点" ) ) == nullptr )
 			return false;
