3c85b9ef0af2045e81a184ffbd1cfae5b749e14b
Acreate <1552397900@qq.com>
2026-01-14 11:58:49 +0800

    添加缺失源码

---
+++ srcs/cpp/MagicRenderView/srcs/node/node/imp/processNodes/baseOperation/self/color/colorArraySelfAddNode.cpp
@@ -0,0 +1,48 @@
+﻿#include "colorArraySelfAddNode.h"
+
+#include <director/varDirector.h>
+#include <node/port/inputPort/array/colorVectorInputPort.h>
+#include <node/port/outputPort/unity/colorOutputPort.h>
+
+#include <tools/imageTools.h>
+
+ColorArraySelfAddNode::ColorArraySelfAddNode( const QString &node_name ) : ProcessNode( node_name ) {
+	outputVarPtr = nullptr;
+}
+bool ColorArraySelfAddNode::initEx( MainWidget *parent ) {
+	initExCallFunction = [this] ( MainWidget *draw_node_widget ) {
+		Def_AppendInputPortType( tr( "颜色序列" ), firstInputPort );
+		Def_AppendBindVarOutputPortType( tr( "结果" ), outputPort, outputVarPtr );
+		return true;
+	};
+	return ProcessNode::initEx( parent );
+}
+bool ColorArraySelfAddNode::updateLayout( ) {
+	if( ProcessNode::updateLayout( ) == false )
+		return false;
+	return true;
+}
+bool ColorArraySelfAddNode::readyNodeRunData( ) {
+	return true;
+}
+bool ColorArraySelfAddNode::fillNodeCall( const QDateTime &ndoe_run_start_data_time, size_t current_frame ) {
+	OutputPort *const*outputPortArray;
+	size_t count;
+	size_t index;
+	NodeType *secondConverPtr;
+	void *portVarPtr;
+	VarDirector *varDirector;
+	const std::vector< OutputPort * > *outputPorts = &getRefPort( firstInputPort );
+	count = outputPorts->size( );
+	if( count == 0 )
+		return true;
+	outputPortArray = outputPorts->data( );
+	for( index = 0; index < count; index += 1 ) {
+		portVarPtr = outputPortArray[ index ]->getVarPtr( );
+		varDirector = outputPortArray[ index ]->getVarDirector( );
+		if( varDirector->cast_ptr( portVarPtr, secondConverPtr ) == false )
+			continue;
+		ImageTools::colorOperation::add( *outputVarPtr, *secondConverPtr );
+	}
+	return true;
+}
---
+++ srcs/cpp/MagicRenderView/srcs/node/node/imp/processNodes/baseOperation/self/int/intArraySelfAddNode.cpp
@@ -0,0 +1,48 @@
+﻿#include "intArraySelfAddNode.h"
+
+#include <director/varDirector.h>
+#include <node/port/inputPort/array/intVectorInputPort.h>
+#include <node/port/outputPort/array/intVectorOutputPort.h>
+#include <node/port/outputPort/unity/intOutputPort.h>
+
+IntArraySelfAddNode::IntArraySelfAddNode( const QString &node_name ) : ProcessNode( node_name ) {
+	outputVarPtr = nullptr;
+}
+bool IntArraySelfAddNode::initEx( MainWidget *parent ) {
+	initExCallFunction = [this] ( MainWidget *draw_node_widget ) {
+		Def_AppendInputPortType( tr( "整数序列" ), firstInputPort );
+		Def_AppendBindVarOutputPortType( tr( "结果" ), outputPort, outputVarPtr );
+		return true;
+	};
+	return ProcessNode::initEx( parent );
+
+}
+bool IntArraySelfAddNode::updateLayout( ) {
+	if( ProcessNode::updateLayout( ) == false )
+		return false;
+	return true;
+}
+bool IntArraySelfAddNode::readyNodeRunData( ) {
+	return true;
+}
+bool IntArraySelfAddNode::fillNodeCall( const QDateTime &ndoe_run_start_data_time, size_t current_frame ) {
+	OutputPort *const*outputPortArray;
+	size_t count;
+	size_t index;
+	NodeType *secondConverPtr;
+	void *portVarPtr;
+	VarDirector *varDirector;
+	const std::vector< OutputPort * > *outputPorts = &getRefPort( firstInputPort );
+	count = outputPorts->size( );
+	if( count == 0 )
+		return true;
+	outputPortArray = outputPorts->data( );
+	for( index = 0; index < count; index += 1 ) {
+		portVarPtr = outputPortArray[ index ]->getVarPtr( );
+		varDirector = outputPortArray[ index ]->getVarDirector( );
+		if( varDirector->cast_ptr( portVarPtr, secondConverPtr ) == false )
+			continue;
+		*outputVarPtr += *secondConverPtr;
+	}
+	return true;
+}
