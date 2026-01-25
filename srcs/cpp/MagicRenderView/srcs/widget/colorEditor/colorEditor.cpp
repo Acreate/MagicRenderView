df6285960b6eba62e037ce125d9c4ac233530476
Acreate <1552397900@qq.com>
2026-01-25 21:50:55 +0800

    备份

--- srcs/cpp/MagicRenderView/srcs/widget/colorEditor/colorEditor.cpp
+++ srcs/cpp/MagicRenderView/srcs/widget/colorEditor/colorEditor.cpp
@@ -31,70 +31,128 @@ void ColorEditor::setCurrentColor( const QColor &current_color ) {
 	currentColor = current_color;
 	repaint( ); // 属性面板
 }
-bool ColorEditor::eventFilter( QObject *watched, QEvent *event ) {
-	auto type = event->type( );
+bool ColorEditor::affirmButtonRelease( ) {
+	emit selectorColor( currentColor );
+	hide( );
+	return true;
+}
+bool ColorEditor::cancelButtonRelease( ) {
+	hide( );
+	return true;
+}
+bool ColorEditor::alphaScrollButtonRelease( ) {
+	return true;
+}
+bool ColorEditor::alphaScrollButtonPaint( ) {
 	QPainter *painter;
-	QMouseEvent *mouseEventPtr;
 	int drawXPos;
 	int height;
 	int width;
 	double value;
+	// 高度
+	height = alphaScrollButton->height( );
+	// 宽度
+	width = alphaScrollButton->width( );
+	// 角度
+	value = 45;
+	// 因子
+	value = value * ( M_PI / 180.0 );
+	// tan 值
+	value = std::tan( value );
+	// 相差像素
+	value = height / value;
+	// 间隔
+	nextInterval = 4;
+	painter = new QPainter( alphaScrollButton );
+	painter->setPen( Qt::GlobalColor::red );
+	drawXPos = 0;
+	width += width; // 增加底部宽度
+	while( true ) {
+		painter->drawLine( drawXPos, 0, drawXPos - value, height );
+		if( drawXPos > width )
+			break;
+		drawXPos += nextInterval;
+	}
+	delete painter;
+	return true;
+}
+bool ColorEditor::eventFilter( QObject *watched, QEvent *event ) {
+	auto type = event->type( );
+	QMouseEvent *mouseEventPtr;
 	switch( type ) {
 		case QEvent::MouseButtonRelease :
 			Reinterpret_Cast( mouseEventPtr, event );
-			if( watched == affirmBtn ) {
-				emit selectorColor( currentColor );
-				hide( );
-				return true;
-			} else if( watched == cancelBen ) {
-				hide( );
-				return true;
-			} else if( watched == alphaScrollButton )
-				return true; // 滑动窗口
+			if( watched == affirmBtn )
+				return affirmButtonRelease( );
+			else if( watched == cancelBen )
+				return cancelButtonRelease( );
+			else if( watched == alphaScrollButton )
+				return alphaScrollButtonRelease( ); // 滑动窗口
 			return true; // 已经处理的即返回
 		case QEvent::Paint :
-			if( alphaScrollButton != watched ) {
-				// 高度
-				height = alphaScrollButton->height( );
-				// 宽度
-				width = alphaScrollButton->width( );
-				// 角度
-				value = 45;
-				// 因子
-				value = value * ( M_PI / 180.0 );
-				// tan 值
-				value = std::tan( value );
-				// 相差像素
-				value = height / value;
-				// 间隔
-				nextInterval = 4;
-				painter = new QPainter( alphaScrollButton );
-				painter->setPen( Qt::GlobalColor::red );
-				drawXPos = 0;
-				width += width; // 增加底部宽度
-				while( true ) {
-					painter->drawLine( drawXPos, 0, drawXPos - value, height );
-					if( drawXPos > width )
-						break;
-					drawXPos += nextInterval;
-				}
-				delete painter;
-				return true;
-			}
-			break;
+			if( alphaScrollButton != watched || alphaScrollButtonPaint( ) == false )
+				return false;
+			return false;
 	}
 	return QWidget::eventFilter( watched, event );
+}
+bool ColorEditor::updateLayout( ) {
+	int width = currentSize.width( );
+	int height = currentSize.height( );
+	int affirmHeight = affirmBtn->height( );
+	int affirmWidth = affirmBtn->width( );
+	int falfWidth = width / 2;
+	falfWidth = falfWidth - affirmWidth;
+	height = height - affirmHeight; // 剩余高度
+	affirmBtn->move( falfWidth, height );
+	cancelBen->move( falfWidth + 5, height );
+	height = height - alphaScrollButton->height( );// 剩余高度
+	double alphaXPos = alphaScrollButton->x( ); // 未移动的 x
+	alphaXPos = alphaXPos / oldSize.width( ); // 旧 x 位置百分比
+	alphaXPos = alphaXPos * currentSize.width( ); // 新 x 位置百分比
+	alphaScrollButton->move( alphaXPos, height ); // 移动到匹配位置
+
+	return true;
+}
+bool ColorEditor::updateColorInfo( ) {
+	return true;
+}
+bool ColorEditor::clearColorInfo( ) {
+	return true;
+}
+bool ColorEditor::reDrawWidget( ) {
+	return true;
+}
+bool ColorEditor::hideEventCall( ) {
+	if( clearColorInfo( ) == false )
+		return false;
+	return true;
+}
+bool ColorEditor::showEventCall( ) {
+	if( updateLayout( ) == false )
+		return false;
+	if( updateColorInfo( ) == false )
+		return false;
+	return true;
 }
 bool ColorEditor::event( QEvent *event ) {
 	auto type = event->type( );
 	switch( type ) {
 		case QEvent::Show :
+			if( showEventCall( ) == false )
+				return false;
 			return true;
 		case QEvent::Hide :
+			if( hideEventCall( ) == false )
+				return false;
 			return true;
 		case QEvent::Paint :
+			if( reDrawWidget( ) == false )
+				return false;
 			return true;
 		case QEvent::Resize :
+			if( updateLayout( ) == false )
+				return false;
 			return true;
 	}
 	return QWidget::event( event );
--- srcs/cpp/MagicRenderView/srcs/widget/colorEditor/colorEditor.h
+++ srcs/cpp/MagicRenderView/srcs/widget/colorEditor/colorEditor.h
@@ -28,6 +28,10 @@ protected:
 	QColor currentColor;
 	/// @brief 临时颜色
 	QColor buffColor;
+	/// @brief 旧的大小
+	QSize oldSize;
+	/// @brief 新的大小
+	QSize currentSize;
 protected Q_SLOTS:
 	virtual void affirmBtenClick( );
 	virtual void cancelBtenClick( );
@@ -37,6 +41,37 @@ public:
 	virtual const QColor & getCurrentColor( ) const { return currentColor; }
 	virtual const QColor & getBuffColor( ) const { return buffColor; }
 	virtual void setCurrentColor( const QColor &current_color );
+protected:
+	/// @brief 更新组件布局
+	/// @return 成功返回 true
+	virtual bool updateLayout( );
+	/// @brief 更新颜色信息到组件
+	/// @return 成功返回 true
+	virtual bool updateColorInfo( );
+	/// @brief 清理组件颜色信息
+	/// @return 成功返回 true
+	virtual bool clearColorInfo( );
+	/// @brief 渲染组件
+	/// @return 成功返回 true
+	virtual bool reDrawWidget( );
+	/// @brief 确定按钮点击调用
+	/// @return 成功执行返回 true
+	virtual bool affirmButtonRelease( );
+	/// @brief 取消按钮点击调用
+	/// @return 成功返回 true
+	virtual bool cancelButtonRelease( );
+	/// @brief 滑动按钮被点击
+	/// @return 调用成功返回 true
+	virtual bool alphaScrollButtonRelease( );
+	/// @brief 滑动按钮绘制事件
+	/// @return 调用成功返回 true
+	virtual bool alphaScrollButtonPaint( );
+	/// @brief 隐藏事件被调用
+	/// @return 成功调用返回 true
+	virtual bool hideEventCall( );
+	/// @brief 隐藏事件被调用
+	/// @return 成功调用返回 true
+	virtual bool showEventCall( );
 protected:
 	bool eventFilter( QObject *watched, QEvent *event ) override;
 	bool event( QEvent *event ) override;
