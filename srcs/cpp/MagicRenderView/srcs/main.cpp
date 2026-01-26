f4250565f8db10479dfd70ce9b4864f53e864a08
Acreate <1552397900@qq.com>
2026-01-27 07:48:48 +0800

    改进调试

--- srcs/cpp/MagicRenderView/srcs/main.cpp
+++ srcs/cpp/MagicRenderView/srcs/main.cpp
@@ -1,10 +1,26 @@
 #include <mainCallFunction.h>
+// 内存泄漏检查开关
+
+// #define CRTDBG
+
 #include <cmake_include_to_c_cpp_header_env.h>
-//#define _CRTDBG_MAP_ALLOC
-//#include <crtdbg.h>
+#if Builder_Tools_MSVC
+	#ifdef CRTDBG
+		#define _CRTDBG_MAP_ALLOC
+		#include <crtdbg.h>
+	#else
+		#define _CrtSetReportMode(...)
+		#define _CrtDumpMemoryLeaks(...)
+	#endif
+#endif
+
 int main( int argc, char *argv[ ], char *envp[ ] ) {
-	//_CrtSetReportMode( _CRT_WARN, _CRTDBG_MODE_DEBUG );
+
+	_CrtSetReportMode( _CRT_WARN, _CRTDBG_MODE_DEBUG );
+
 	int exec = MainCallFunction::runCode( argc, argv, envp );
-	//_CrtDumpMemoryLeaks( );
+
+	_CrtDumpMemoryLeaks( );
+
 	return exec;
 }
