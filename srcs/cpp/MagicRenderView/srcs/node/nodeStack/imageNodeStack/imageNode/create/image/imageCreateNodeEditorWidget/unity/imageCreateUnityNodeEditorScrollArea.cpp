9740156b200f2cbd824abb9095e7e3a3b0c53b16
Acreate <1552397900@qq.com>
2026-01-27 17:47:04 +0800

    图像创建功能

--- srcs/cpp/MagicRenderView/srcs/node/nodeStack/imageNodeStack/imageNode/create/image/imageCreateNodeEditorWidget/unity/imageCreateUnityNodeEditorScrollArea.cpp
+++ srcs/cpp/MagicRenderView/srcs/node/nodeStack/imageNodeStack/imageNode/create/image/imageCreateNodeEditorWidget/unity/imageCreateUnityNodeEditorScrollArea.cpp
@@ -1,17 +1,50 @@
 #include "imageCreateUnityNodeEditorScrollArea.h"
 
+#include <app/application.h>
+#include <QDir>
+#include <QFileDialog>
 #include <QLabel>
 #include <QLineEdit>
+#include <qpushbutton.h>
 #include <QVBoxLayout>
 
 ImageCreateUnityNodeEditorScrollArea::ImageCreateUnityNodeEditorScrollArea( NodeInfoWidget *parent, const QImage &current_var ) : EditorNodeInfoScrollArea( parent ), currentVar( current_var ) {
+	fileDialog = new QFileDialog( this );
+	fileDialog->setWindowTitle( tr( "选择加载图像..." ) );
+	fileDialog->setNameFilter( tr( "图像 (*.png)" ) );
 	mainWidget = new QWidget( this );
 	setWidget( mainWidget );
 	mainLayout = new QVBoxLayout( mainWidget );
 
+	buttonCompoentWidget = new QWidget( mainWidget );
+	QHBoxLayout *buttonCompoentLayout = new QHBoxLayout( buttonCompoentWidget );
+	pathLoadFileButton = new QPushButton( tr( "加载图像" ), buttonCompoentWidget );
+	buttonCompoentLayout->addWidget( pathLoadFileButton );
+	connect( pathLoadFileButton, &QPushButton::clicked, this, &ImageCreateUnityNodeEditorScrollArea::onLoadPng );
+	mainLayout->addWidget( buttonCompoentWidget );
+
+	showImageLabel = new QLabel( mainWidget );
+	mainLayout->addWidget( showImageLabel );
 }
 void ImageCreateUnityNodeEditorScrollArea::releaseResource( ) {
 	EditorNodeInfoScrollArea::releaseResource( );
+}
+bool ImageCreateUnityNodeEditorScrollArea::eventFilter( QObject *watched, QEvent *event ) {
+	return EditorNodeInfoScrollArea::eventFilter( watched, event );
+}
+void ImageCreateUnityNodeEditorScrollArea::onLoadPng( ) {
+	if( lastLoadFilePath.isEmpty( ) )
+		lastLoadFilePath = QDir::currentPath( );
+	fileDialog->exec( );
+	auto selectedFiles = fileDialog->selectedFiles( );
+	if( selectedFiles.size( ) == 0 )
+		return;
+	lastLoadFilePath = selectedFiles.first( );
+	if( currentVar.load( lastLoadFilePath ) == false )
+		return;
+	auto fromImage = QPixmap::fromImage( currentVar );
+	showImageLabel->setPixmap( fromImage );
+	emit editingFinished_Signal( currentVar );
 }
 
 bool ImageCreateUnityNodeEditorScrollArea::initNode( Node *init_node ) {
@@ -24,4 +57,6 @@ ImageCreateUnityNodeEditorScrollArea::~ImageCreateUnityNodeEditorScrollArea( ) {
 }
 void ImageCreateUnityNodeEditorScrollArea::setCurrentVar( const QImage &current_var ) {
 	currentVar = current_var.copy( );
+	auto fromImage = QPixmap::fromImage( currentVar );
+	showImageLabel->setPixmap( fromImage );
 }
--- srcs/cpp/MagicRenderView/srcs/node/nodeStack/imageNodeStack/imageNode/create/image/imageCreateNodeEditorWidget/unity/imageCreateUnityNodeEditorScrollArea.h
+++ srcs/cpp/MagicRenderView/srcs/node/nodeStack/imageNodeStack/imageNode/create/image/imageCreateNodeEditorWidget/unity/imageCreateUnityNodeEditorScrollArea.h
@@ -3,20 +3,30 @@
 
 #include <node/nodeInfoWidget/mainInfoWidget/editorNodeInfoScrollArea.h>
 
+class QFileDialog;
+class QLabel;
+class QPushButton;
 class ValidatorWidget;
 class QVBoxLayout;
 class ImageCreateUnityNodeEditorScrollArea : public EditorNodeInfoScrollArea {
 	Q_OBJECT;
 	friend class ImageCreateUnityNodeEditorWidget;
 protected:
+	QFileDialog *fileDialog;
+	QString lastLoadFilePath;
 	QImage currentVar;
 	QWidget *mainWidget;
 	QVBoxLayout *mainLayout;
 	ValidatorWidget *currentEditorValidator;
 	std::vector< ValidatorWidget * > lineFinishedEditorVector;
+	QWidget *buttonCompoentWidget;
+	QPushButton *pathLoadFileButton;
+	QLabel *showImageLabel;
 protected:
 	ImageCreateUnityNodeEditorScrollArea( NodeInfoWidget *parent, const QImage &current_var );
 	void releaseResource( ) override;
+	bool eventFilter( QObject *watched, QEvent *event ) override;
+	void onLoadPng( );
 public:
 	bool initNode( Node *init_node ) override;
 	~ImageCreateUnityNodeEditorScrollArea( ) override;
